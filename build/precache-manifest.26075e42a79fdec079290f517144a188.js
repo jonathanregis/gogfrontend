19dbceb9c4ce8fe7d9191b0547ea742caf3c02fc
jonathan regis <imjonathan.r@gmail.com>
2020-02-15 02:27:22 +0000

    include build folder

---
+++ build/precache-manifest.26075e42a79fdec079290f517144a188.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164f6f98b400d077aed8f9b6b95aa1b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c9e0abec586d53207d1",
+    "url": "/static/css/main.6b49b840.chunk.css"
+  },
+  {
+    "revision": "373f5e4dff9b96ead9ec",
+    "url": "/static/js/2.9d23fa11.chunk.js"
+  },
+  {
+    "revision": "98061d712500f07672e3581cfdcd5a24",
+    "url": "/static/js/2.9d23fa11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c9e0abec586d53207d1",
+    "url": "/static/js/main.1e859c9c.chunk.js"
+  },
+  {
+    "revision": "d55402854712ef4f7a20",
+    "url": "/static/js/runtime-main.a3c5c833.js"
+  },
+  {
+    "revision": "c67c18a6cb20ddfdeae92e9f9dc4b1ad",
+    "url": "/static/media/bg.c67c18a6.png"
+  },
+  {
+    "revision": "c41acd99ea977f5c1619fd8ad71051db",
+    "url": "/static/media/img.c41acd99.png"
+  },
+  {
+    "revision": "00d90eadc9fa2501b255447b8442255f",
+    "url": "/static/media/img_0.00d90ead.png"
+  },
+  {
+    "revision": "f1b444dfc5431ab5a23925fd8b0d5269",
+    "url": "/static/media/img_1.f1b444df.png"
+  },
+  {
+    "revision": "d6ccff5ca2e44f354e4f0ceecc26a766",
+    "url": "/static/media/img_2.d6ccff5c.png"
+  },
+  {
+    "revision": "f3cd001e8c20753d5165986d99d249db",
+    "url": "/static/media/img_3.f3cd001e.png"
+  }
+]);
